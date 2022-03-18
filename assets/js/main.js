65a3ce683ec06c84d109ef34b5158e57aa4469af
Kaitlin Newson <kaitlin.newson@utoronto.ca>
2022-03-18 16:14:30 -0400

    remove timeline scrolling effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,5 @@
 
 jQuery(document).ready(function($){
-    var timelineBlocks = $('.cd-timeline-block'),
-        offset = 0.8;
-
-    //hide timeline blocks which are outside the viewport
-    hideBlocks(timelineBlocks, offset);
-
-    //on scolling, show/animate timeline blocks when enter the viewport
-    $(window).on('scroll', function(){
-        if (window.requestAnimationFrame) {
-            return window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset) });
-        }
-        return setTimeout(function(){ showBlocks(timelineBlocks, offset) }, 100)
-    });
-
-    function hideBlocks(blocks, offset) {
-        blocks.each(function(){
-            if ($(this).offset().top > $(window).scrollTop() + $(window).height() * offset) {
-                $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden')
-            }
-        });
-    }
-
-    function showBlocks(blocks, offset) {
-        blocks.each(function(){
-            if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset &&
-                $(this).find('.cd-timeline-img').hasClass('is-hidden')) {
-                    $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in')
-                }
-        });
-    }
 
     $('.sort-workshop-time').click(function(){
         console.log('sort event - time')
